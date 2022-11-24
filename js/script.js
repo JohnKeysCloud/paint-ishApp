c962a8a9cca249531721d6cda0da91c34b1c5927
JOHN KEYS CLOUD <johncloud@cyclonestud.io>
2022-11-23 21:25:28 -0500

    initial commit… prepare for launch

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const canvas = document.getElementById('canvas');
+let pixelSetting = 16;
+
+// pixelCreation
+for (let i = 0; i < pixelSetting; ++i) {
+	let pixelBorder = document.createElement('div');
+	pixelBorder.classList.add('pixel-border');
+
+	let pixel = document.createElement('div');
+	pixel.classList.add('pixel');
+
+	pixelBorder.appendChild(pixel);	
+	canvas.appendChild(pixelBorder);
+}
+
+// pixelRaiseOpacity
+let pixels = document.getElementsByClassName('pixel');
+for (let i = 0; i <= pixels.length; ++i) {
+	let opacity = 0;
+
+	function paintIsh(e) {
+		if (opacity === 1) return;
+		console.log(e.target);
+		e.target.style.opacity = opacity += 0.2;
+	}
+
+	pixels[i].addEventListener('mouseenter', paintIsh);
+}
+
+
+
+
+
+
+
+
+
+
+
+
