fe92aedfbc7e8e236f898fc5f010fda371b1d2a0
JOHN KEYS CLOUD <johncloud@cyclonestud.io>
2022-12-01 21:14:10 -0500

    start enabling eraser functionality, refactor css selectors for buttons

--- js/script.js
+++ js/script.js
@@ -6,8 +6,113 @@ const sliderOutput = document.getElementById('slider-output');
 const gridBackgroundAdjuster = document.querySelector('#center-page main #canvas');
 const pixelColorAdjuster = document.getElementById('color-pixels');
 const canvasColorAdjuster = document.getElementById('canvas-background-color');
+const eraseToggleBtn = document.getElementById('erasing-toggle');
+const shadeToggleBtn = document.getElementById('shading-toggle');
+const startOverBtn = document.getElementById('start-over-btn');
 
 let oneAxisLength = 16;
+
+function sketchShade(e) {
+	let currentOpacity = +e.target.style.opacity;
+	let initialOpacity = 0.2;
+
+	if (!e.target.classList.contains('pixel')) return;
+	if (currentOpacity === 1) return;
+
+	e.target.style.opacity = initialOpacity += currentOpacity;
+}
+
+function sketchPen(e) {
+	e.target.style.opacity = 1;
+}
+canvas.addEventListener('mouseover', sketchPen);
+
+function toggleEraser(e) {
+	let dataSketchMethod = canvas.getAttribute('data-sketch-method');
+
+	e.target.classList.toggle('enabled');
+
+	if (e.target.classList.contains('enabled')) {
+		e.target.style.transform = 'scale(1.1)';
+
+		if (dataSketchMethod === 'pen') {
+			console.log('remove pen listener')
+			canvas.removeEventListener('mouseover', sketchPen);
+
+			function erasePen(e) {
+				let currentOpacity = +e.target.style.opacity;
+
+				if (!e.target.classList.contains('pixel')) return;
+				if (currentOpacity === 0) return;
+			
+				e.target.style.opacity = 0;
+			}
+
+			canvas.addEventListener('mouseover', erasePen)
+		} else {
+			console.log('remove shade listener');
+			canvas.removeEventListener('mouseover', sketchShade);
+		
+			function eraseShade(e) {
+				let currentOpacity = +e.target.style.opacity;
+				let initialOpacity = 0.2;
+
+				if (!e.target.classList.contains('pixel')) return;
+				if (currentOpacity === 0) return;
+			
+				e.target.style.opacity = currentOpacity -= initialOpacity;
+			}
+
+			canvas.addEventListener('mouseover', eraseShade)
+		}
+	} else {
+		e.target.style.transform = 'scale(1)';
+
+		if (dataSketchMethod === 'pen') {
+			canvas.removeEventListener('mouseover', erasePen);
+		} else {
+			canvas.removeEventListener('mouseover', eraseShade);
+		}
+
+		if (dataSketchMethod === 'pen') {
+			canvas.addEventListener('mouseover', sketchPen);
+		} else {
+			canvas.addEventListener('mouseover', sketchShade);
+		}
+	}
+} 
+eraseToggleBtn.addEventListener('click', toggleEraser)
+
+function toggleShade(e) {
+	let dataSketchMethod = canvas.getAttribute('data-sketch-method');
+
+	e.target.classList.toggle('enabled');
+	
+	canvas.removeEventListener('mouseover', sketchShade);
+	canvas.removeEventListener('mouseover', sketchPen);
+	
+	if (e.target.classList.contains('enabled')) {
+		canvas.removeEventListener('mouseover', sketchPen);
+		canvas.setAttribute('data-sketch-method', 'shade');
+		canvas.addEventListener('mouseover', sketchShade);
+	} else {
+		canvas.removeEventListener('mouseover', sketchShade);
+		canvas.setAttribute('data-sketch-method', 'pen');
+		canvas.addEventListener('mouseover', sketchPen);
+	}
+}
+shadeToggleBtn.addEventListener('click', toggleShade);
+		
+function clearCanvas() {
+	for (let i = 0; i < pixels.length; ++i) {
+		pixels[i].classList.add('refresh');
+		pixels[i].addEventListener('animationend', () => {
+			pixels[i].classList.remove('refresh');
+			pixels[i].style.setProperty('opacity', 0);
+		});
+	}
+}
+startOverBtn.addEventListener('click', clearCanvas)
 
 function canvasColorSwitch() {
 	console.log('canvasColor Switch Event Triggered 💭');
@@ -25,18 +130,7 @@ function pixelColorSwitch() {
 }
 pixelColorAdjuster.addEventListener('input', pixelColorSwitch);
 
-function pixelRaiseOpacity(e) {
-	let currentOpacity = +e.target.style.opacity;
-	let initialOpacity = 0.2;
-
-	if (!e.target.classList.contains('pixel')) return;
-	if (currentOpacity === 1) return;
-
-	e.target.style.opacity = initialOpacity += currentOpacity;
-}
-canvas.addEventListener('mouseover', pixelRaiseOpacity);
-
-function pixelCreation() {
+function generatePixels() {
 	let dimensions = oneAxisLength ** 2;
 
 	for (let i = 0; i < dimensions; ++i) {
@@ -51,7 +145,7 @@ function pixelCreation() {
 	}
 }
 
-function pixelDestruction() {
+function destroyPixels() {
 	while (canvas.firstChild) {
 		canvas.removeChild(canvas.firstChild);
 	}
@@ -59,7 +153,7 @@ function pixelDestruction() {
 	oneAxisLength = +slider.value;
 	root.style.setProperty('--grid-fraction', oneAxisLength);
 
-	pixelCreation();
+	generatePixels();
 }
 
 function updateSliderValue() {
@@ -68,8 +162,8 @@ function updateSliderValue() {
 
 function createGrid() {
 	updateSliderValue()
-	pixelDestruction();
+	destroyPixels();
 }
-createGrid();
 
+createGrid();
 slider.addEventListener('input', createGrid);
